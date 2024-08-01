ec89e7320a8dc8bf13971e4339fe09d696c82cde
madhav <163291389+madhavsGIT@users.noreply.github.com>
2024-08-01 22:45:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn")
+
+btn.addEventListener('click', async () => {
+	const text = document.getElementById("text").value;
+	const delay = parseInt(document.getElementById("delay").value);
+	await delayFunction(delay);
+	document.getElementById("output").innerText = text;
+})
+
+function dealyFunction(delay) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, delay)
+	})
+}
